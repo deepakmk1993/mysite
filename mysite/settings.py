dd2ddef9a4aa73cc1c650ba7bdcd3decae814739
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-07 16:15:49 +0000

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,10 +79,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'ENGINE': 'django.db.backends.postgresql',
+	'NAME': '',
+	'USER': '',
+	'PASSWORD': '',
+	'HOST': '',
+	'PORT': '',
 }
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
