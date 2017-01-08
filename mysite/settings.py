7e0b6763affb83d63f60fc6ae4a39b78f9cf7ab1
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-08 03:37:29 +0000

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,8 +123,3 @@ USE_TZ = True
 STATIC_ROOT = "static"
 
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
