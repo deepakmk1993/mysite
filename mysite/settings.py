d15819e0e56092902cea883d345e2786cc840a0a
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-08 03:40:04 +0000

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = "static"
 
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    "app/static",
+)
