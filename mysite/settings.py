40c8f97470eb2d3fcc38ff2708669ce3399fe4ae
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-08 03:49:03 +0000

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
