9b2de5a2351117615f2b151f47f5324f42907e45
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-08 01:55:42 +0000

    first commit

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
 def view_posts(request):
-	posts = Post.objects.all().order_by("create_time");
+	post_list = Post.objects.all().order_by("create_time");
+	paginator = Paginator(post_list, 5) # Show 5 posts per page
+
+	page = request.GET.get('page')
+	try:
+	    posts = paginator.page(page)
+	except PageNotAnInteger:
+	    # If page is not an integer, deliver first page.
+	    posts = paginator.page(1)
+	except EmptyPage:
+	    # If page is out of range (e.g. 9999), deliver last page of results.
+	    posts = paginator.page(paginator.num_pages)
+
 	return render(request, 'posts.html', {'posts': posts});
 
 def view_post(request, pk):
