1f8983c21fc53ade6851ad39ebedd18875ed3dc7
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-08 03:52:29 +0000

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    	'ENGINE': 'django.db.backends.postgresql',
+    	'NAME': '',
+    	'USER': '',
+    	'PASSWORD': '',
+    	'HOST': '',
+    	'PORT': '',
     }
 }
 import dj_database_url
