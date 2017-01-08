230369326108798a02628d6d2442612cf2fd1484
Deepak <deepakgupta.ultimate@gmail.com>
2017-01-08 04:03:48 +0000

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,8 +87,7 @@ DATABASES = {
     }
 }
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
